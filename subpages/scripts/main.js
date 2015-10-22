4c92bbcf62f1a13d271d49b7bfc9f3676bf25507
rafaeld6 <rafael.d6interactive@gmail.com>
2015-10-22 14:59:24 +0800

    Add subpages desesign

---
+++ subpages/scripts/main.js
@@ -0,0 +1,15 @@
+
+
+$(window).load(function() { // makes sure the whole site is loaded
+	$('#status').fadeOut(); // will first fade out the loading animation
+	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(350).css({'overflow':'visible'}).addClass('is-shown');
+})
+
+$('.menu-toggle').click(function() {
+    $('body').addClass('is-in');
+});
+
+$('.menu-navbar').click(function() {
+    $('body').removeClass('is-in');
+});
